3a63a88982f6932dba037323b04aac74343912ae
Peter Bastyi <bastyip@starschema.net>
2016-03-17 15:20:47 +0100

    Fixed watchdog log path and service discovery is now always retrieving Palette Insight products version instead of only Palette

--- watchdog/main.go
+++ watchdog/main.go
@@ -119,8 +119,7 @@ func main() {
 
 	// Initialize the log to write into file instead of stderr
 	// open output file
-	os.Mkdir("Logs", 777)
-	logFileName := "Logs/watchdog.log"
+	logFileName := os.Args[0] + ".log"
 	logFile, err := os.OpenFile(logFileName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
 	if err != nil {
 		fmt.Println("Failed to open log file! ", err)
